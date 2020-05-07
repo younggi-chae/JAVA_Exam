6c25c5949b3441cc09b68e0d974d352e68692a7b
YoungGi <dudrl4612@naver.com>
2020-05-07 21:42:02 +0900

    loop

---
+++ src/ex/loop/loopEx2.java
@@ -0,0 +1,16 @@
+package ex.loop;
+
+public class loopEx2 {
+	// 별찍기 연습
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print("*"); // *출력
+			}
+			System.out.println(); // 줄바꿈
+		}
+
+	}
+
+}
