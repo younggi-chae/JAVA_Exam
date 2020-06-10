cef7a0d2416dd88fde2ad342c31394f6f11164c9
YoungGi <dudrl4612@naver.com>
2020-06-10 10:26:49 +0900

    DataStream

---
+++ src/ex/I_O/Data_IO_StreamEx2.java
@@ -0,0 +1,26 @@
+package ex.I_O;
+
+import java.io.*;
+
+public class Data_IO_StreamEx2 {
+
+	public static void main(String[] args) {
+		int[] score = { 100, 90, 95, 85, 50 };
+
+		try {
+			FileOutputStream fos = new FileOutputStream("score.dat");
+			DataOutputStream dos = new DataOutputStream(fos);
+
+			for (int i = 0; i < score.length; i++) {
+				dos.writeInt(score[i]);
+
+			}
+
+			dos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
