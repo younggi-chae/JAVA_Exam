3b8fac2041d2e7a49f1e13d35889814939887534
YoungGi <dudrl4612@naver.com>
2020-06-15 10:57:17 +0900

    DAO

--- Board_MVC/src/main/java/mvc/board/impl/BoardDAO.java
+++ Board_MVC/src/main/java/mvc/board/impl/BoardDAO.java
@@ -1,22 +1,21 @@
 package mvc.board.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import mvc.board.BoardDTO;
-import mvc.common.JDBCUtil;
 
 @Repository("boardDAO")
 public class BoardDAO {
 	
-	private Connection conn = null;
-	private PreparedStatement stmt = null;
-	private ResultSet rs = null;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	private final String BOARD_INSERT = "insert into board(SEQ, TITLE, WRITER, CONTENT) "
 			+ "values((select ifnull(max(seq),0)+1 from board as a),?,?,?)";
@@ -28,104 +27,45 @@ public class BoardDAO {
 	//글등록
 	public void insertBoard(BoardDTO dto) {
 		System.out.println("===> JDBC로 insertBoard() 기능 처리");
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_INSERT);
-			stmt.setString(1, dto.getTitle());
-			stmt.setString(2, dto.getWriter());
-			stmt.setString(3, dto.getContent());
-			stmt.executeUpdate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(stmt, conn);
-		}
+		jdbcTemplate.update(BOARD_INSERT, dto.getTitle(), dto.getWriter(), dto.getContent());
 	}
 	
 	//글수정
 	public void updateBoard(BoardDTO dto) {
 		System.out.println("===> JDBC로 updateBoard() 기능 처리");
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_UPDATE);
-			stmt.setString(1, dto.getTitle());
-			stmt.setString(2, dto.getContent());
-			stmt.setInt(3, dto.getSeq());
-			stmt.executeUpdate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(stmt, conn);
-		}
+		jdbcTemplate.update(BOARD_UPDATE, dto.getTitle(), dto.getContent(), dto.getSeq());
 	}
 	
 	//글삭제
 	public void deleteBoard(BoardDTO dto) {
 		System.out.println("===> JDBC로 deleteBoard() 기능 처리");
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_DELETE);
-			stmt.setInt(1, dto.getSeq());
-			stmt.executeUpdate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(stmt, conn);
-		}
+		jdbcTemplate.update(BOARD_DELETE, dto.getSeq());
 	}
 	
 	//글상세 조회
 	public BoardDTO getBoard(BoardDTO dto) {
 		System.out.println("===> JDBC로 getBoard() 기능 처리");
-		BoardDTO board = null;
-		
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_GET);
-			stmt.setInt(1, dto.getSeq());
-			rs = stmt.executeQuery();
-			if(rs.next()) {
-				board = new BoardDTO();
-				board.setSeq(rs.getInt("SEQ"));
-				board.setTitle(rs.getString("TITLE"));
-				board.setWriter(rs.getString("WRITER"));
-				board.setContent(rs.getString("CONTENT"));
-				board.setRegDate(rs.getDate("REGDATE"));
-				board.setCnt(rs.getInt("CNT"));					
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(rs, stmt, conn);
-		}
-		return board;
+		Object[] args = {dto.getSeq()};	
+		return jdbcTemplate.queryForObject(BOARD_GET, args, new BoardRowMapper());
 	}
 	
 	//글 목록 조회
 	public List<BoardDTO> getBoardList(BoardDTO dto) {
 		System.out.println("===> JDBC로 getBoardList() 기능 처리");
-		List<BoardDTO> boardList = new ArrayList<BoardDTO>();
-		
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_LIST);
-			rs = stmt.executeQuery();
-			while(rs.next()) {
-				BoardDTO board = new BoardDTO();
-				board.setSeq(rs.getInt("SEQ"));
-				board.setTitle(rs.getString("TITLE"));
-				board.setWriter(rs.getString("WRITER"));
-				board.setContent(rs.getString("CONTENT"));
-				board.setRegDate(rs.getDate("REGDATE"));
-				board.setCnt(rs.getInt("CNT"));
-				boardList.add(board);				
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(rs, stmt, conn);
-		}
-		return boardList;
+		return jdbcTemplate.query(BOARD_LIST, new BoardRowMapper());
 	}
 	
+}
+
+class BoardRowMapper implements RowMapper<BoardDTO> {
+	public BoardDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		BoardDTO board = new BoardDTO();
+		board.setSeq(rs.getInt("SEQ"));
+		board.setTitle(rs.getString("TITLE"));
+		board.setWriter(rs.getString("WRITER"));
+		board.setContent(rs.getString("CONTENT"));
+		board.setRegDate(rs.getDate("REGDATE"));
+		board.setCnt(rs.getInt("CNT"));
+		return board;
+	}
 }
