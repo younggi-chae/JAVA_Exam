0c76a2f8560ecf7b938dcaa4c34e692677623b0f
YoungGi <dudrl4612@naver.com>
2020-06-22 11:38:08 +0900

    board

---
+++ Board_MVC/src/main/java/mvc/board/BoardService.java
@@ -0,0 +1,22 @@
+package mvc.board;
+
+import java.util.List;
+
+public interface BoardService {
+
+	//글등록
+	void insertBoard(BoardDTO dto); 
+
+	//글수정   
+	void updateBoard(BoardDTO dto);
+	
+	//글삭제
+	void deleteBoard(BoardDTO dto);
+
+	//글상세 조회
+	BoardDTO getBoard(BoardDTO dto);
+
+	//글 목록 조회
+	List<BoardDTO> getBoardList(BoardDTO dto);
+
+}
