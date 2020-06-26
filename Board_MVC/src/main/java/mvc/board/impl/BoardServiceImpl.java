74d769bea0d0d5084742087b826cf42703737097
YoungGi <dudrl4612@naver.com>
2020-06-26 22:34:33 +0900

    Service

--- Board_MVC/src/main/java/mvc/board/impl/BoardServiceImpl.java
+++ Board_MVC/src/main/java/mvc/board/impl/BoardServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import mvc.board.BoardDTO;
 import mvc.board.BoardService;
+import mvc.common.Search;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
@@ -19,7 +20,7 @@ public class BoardServiceImpl implements BoardService {
 	public void insertBoard(BoardDTO dto) {
 		boardDAO.insertBoard(dto);
 	}
-
+ 
 	public void updateBoard(BoardDTO dto) {
 		boardDAO.updateBoard(dto);
 	}
@@ -32,12 +33,17 @@ public class BoardServiceImpl implements BoardService {
 		return boardDAO.getBoard(dto);
 	}
 
-	public List<BoardDTO> getBoardList(BoardDTO dto) {
-		return boardDAO.getBoardList(dto);
+	public List<BoardDTO> getBoardList(Search search) {
+		return boardDAO.getBoardList(search);
 	}
 	
 	@Transactional(isolation = Isolation.READ_COMMITTED)
 	public void updateCnt(BoardDTO dto) {
 		boardDAO.updateCnt(dto);	
 	}
+	
+	public int getBoardListCnt(Search search) {
+		return boardDAO.getBoardListCnt(search);
+	}
+	
 }
