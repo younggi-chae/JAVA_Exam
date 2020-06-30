a850c7adb5dd2b6618a3b3e034cf959ced619c48
YG <zosjs2@gmail.com>
2020-06-30 16:31:21 +0900

    test

---
+++ javaBasic/src/kosta/basic/VariableExam.java
@@ -0,0 +1,18 @@
+package kosta.basic;
+
+public class VariableExam {
+	public static void main(String[] args) {
+		
+		int num1; //변수 선언
+		num1 = 1000000000;
+		
+		float num2 = 3.14f;
+		double num3 = 3.14;
+		
+		char num4 = 'A';
+		String num5 = "ABC";
+		
+		boolean num6 = true;
+		
+	}
+}
---
+++ javaBasic/src/kosta/mission/Mission01.java
@@ -0,0 +1,16 @@
+package kosta.mission;
+
+public class Mission01 {
+	public static void main(String[] args) {
+		
+		String name = "채영기";
+		int age = 30;
+		String address = "인천";
+		String tel = "010-9975-7132";
+		
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("주소 : " + address);
+		System.out.println("전화번호 : " + tel);
+	}	
+}
