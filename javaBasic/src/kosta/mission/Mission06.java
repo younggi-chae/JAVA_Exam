a39b8224a7e68e19d33d64c54aaaceb010867ba0
younggi-chae <dudrl4612@naver.com>
2020-07-03 17:37:12 +0900

    Mission

---
+++ javaBasic/src/kosta/mission/Mission06.java
@@ -0,0 +1,22 @@
+package kosta.mission;
+
+import java.util.Scanner;
+
+public class Mission06 {
+
+	public static void main(String[] args) {
+		
+		//2개의 정수를 입력받아 b-a 결과를 출력하자.
+		//b > a 가 되도록 일력받음
+		//1. do~ while
+		//2. while(true) break;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+	}
+
+}
