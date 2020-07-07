3498008b5a8e1188d52ca097f7ac0ddabc5c5670
younggi-chae <dudrl4612@naver.com>
2020-07-07 17:46:53 +0900

    OOP

--- javaBasic/src/kosta/oop/BookMain.java
+++ javaBasic/src/kosta/oop/BookMain.java
@@ -2,20 +2,22 @@ package kosta.oop;
 
 public class BookMain {
 
+	static int total;
+	
 	public static void main(String[] args) {
 		
 		Book book[] = {new Book("JAVA", 30000),
 					   new Book("JSP", 25000),
 					   new Book("Oracle", 15000)
-		};
-		
+		};		
+				
 		for(int i = 0; i < book.length; i++) {
-		book[i].discount();
-		}
-		
-		for(int i = 0; i < book.length; i++) {
+			book[i].discount();
 			book[i].print();
+			total += book[i].dcPrice;
 		}
+		System.out.println();
+		System.out.println("총금액 : "+ total);
 	}
 }
 
