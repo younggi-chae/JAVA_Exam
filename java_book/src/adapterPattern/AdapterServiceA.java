663933e24eb790c21e6f8fac07aae77bec83ccf3
YoungGi <dudrl4612@naver.com>
2020-07-11 00:41:00 +0900

    AdapterPattern

---
+++ java_book/src/adapterPattern/AdapterServiceA.java
@@ -0,0 +1,14 @@
+package adapterPattern;
+
+public class AdapterServiceA {
+	ServiceA sa1 = new ServiceA();
+
+	
+	void runService() {
+		sa1.runServiceA();
+		
+	}
+
+}
+
+
---
+++ java_book/src/adapterPattern/AdapterServiceB.java
@@ -0,0 +1,10 @@
+package adapterPattern;
+
+public class AdapterServiceB {
+		ServiceB sb1 = new ServiceB();
+		
+		void runService() {
+			sb1.runServiceB();
+		}
+}
+
