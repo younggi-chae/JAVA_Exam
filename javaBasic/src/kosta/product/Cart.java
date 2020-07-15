d9976c075678a069304a6e714c10b22c50a3bf71
younggi-chae <dudrl4612@naver.com>
2020-07-15 17:32:04 +0900

    Product

--- javaBasic/src/kosta/product/Cart.java
+++ javaBasic/src/kosta/product/Cart.java
@@ -1,35 +1,33 @@
 package kosta.product;
 
 public class Cart {
-		
-	private Product[] pArr;	
-	int total;
-	int count;
-
-	public Cart() {
-		pArr = new Product[100];
-	}
-
-	public void addCart(Product p) {
-
+	private Product[] pArr;
+	private int total;
+	int count = 0;
+	
+	
+	public Cart(){
 		pArr = new Product[100];
 	}
 	
-	public void orderList() {
-		
-		for(int i = 0; i < count; i++) {
+	public void addCart(Product p){
+		pArr[count++] = p;
+	}
+	
+	public void orderList(){
+		for(int i=0;i<count;i++){
 			pArr[i].show();
-			System.out.println();			
+			System.out.println();
 		}
 		
-		System.out.println("총 주문금액 : " + getTotal());
+		System.out.println("총 주문금액: " + getTotal());
 	}
 	
-	public int getTotal() {
-		for(int i = 0; i < count; i++) {
+	public int getTotal(){
+		for(int i=0;i<count;i++){
 			total += pArr[i].getPrice() * (1 - pArr[i].getDc());
 		}
+		
 		return total;
 	}
-
 }
