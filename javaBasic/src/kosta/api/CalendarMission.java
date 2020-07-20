5399fa934faf65f02da8204c36de9ba997b7668f
younggi-chae <dudrl4612@naver.com>
2020-07-20 09:23:59 +0900

    Calendar

--- javaBasic/src/kosta/api/CalendarMission.java
+++ javaBasic/src/kosta/api/CalendarMission.java
@@ -12,10 +12,26 @@ public class CalendarMission {
 		//      1 2 3 4
 		//  5 6 7 8 9 10
 		
-		Calendar gc = Calendar.getInstance();
-		gc.set(2020, 6, 1); //7월 1일 이동
-		int n = gc.getActualMaximum(Calendar.DATE);
-		System.out.println(n);
+Calendar gc = Calendar.getInstance();		
+		
+		gc.set(2020, 6, 1);
+		int n = gc.getActualMaximum(Calendar.DATE);		
+		int week = gc.get(Calendar.DAY_OF_WEEK);
+		
+		System.out.println("<2020년 7월>");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		
+		for(int i=1;i<week;i++) {
+			System.out.print("\t");
+		}
+		
+		for(int i=1;i<=n;i++) {
+			System.out.print(i+"\t");
+			if((week + i -1)%7 == 0) {
+				System.out.println();
+			}
+		}
+		
 	}
 
 }
