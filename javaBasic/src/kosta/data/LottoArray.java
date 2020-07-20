c40d059feb87cc26ba10bd12348dff3730949f2c
younggi-chae <62976819+younggi-chae@users.noreply.github.com>
2020-07-20 21:59:55 +0900

    Update LottoArray.java

--- javaBasic/src/kosta/data/LottoArray.java
+++ javaBasic/src/kosta/data/LottoArray.java
@@ -3,18 +3,21 @@ package kosta.data;
 public class LottoArray {
 
 	public static void main(String[] args) {
-
-		int lotto[] = new int[6];
+		int num[] = new int[6];
 
 		for (int i = 0; i < 6; i++) {
-			lotto[i] = (int) (Math.random() * 45) + 1;
-
+			num[i] = (int) (Math.random() * 45) + 1;
 			
+			for (int j = 0; j < i; j++) {
+				if (num[i] == num[j]) {
+					i--;
+					break;
+				}
+			}
 		}
 
 		for (int i = 0; i < 6; i++) {
-			System.out.print(lotto[i] + ", ");
+			System.out.print(num[i] + " ");
 		}
 	}
-
 }
