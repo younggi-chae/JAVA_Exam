e3f28f53a9bfe2d11144d7b0c8f8e4bc577706c8
YoungGi <dudrl4612@naver.com>
2020-07-25 12:09:08 +0900

    aop

---
+++ SpringEx/src/main/java/aop002/MyAspect.java
@@ -0,0 +1,15 @@
+package aop002;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class MyAspect {
+	@Before("execution(* runSomething())")
+	public void before(JoinPoint joinpoint) {
+		System.out.println("얼굴 인식 확인 : 문을 개방하라");
+		//System.out.println("열쇠로 문을 열고 집에 들어간다.");
+	}
+	
+}
---
+++ SpringEx/src/main/java/aop002/Start.java
@@ -0,0 +1,18 @@
+package aop002;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Start {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("aop002.xml",Start.class);
+		
+		Person romeo = context.getBean("boy", Person.class);
+		Person juliet = context.getBean("girl", Person.class);
+		
+		romeo.runSomething();
+
+	}
+
+}
