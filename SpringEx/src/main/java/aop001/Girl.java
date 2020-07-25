ae4fe36014dc3d35f28a96c113fa72119f1b2be7
YoungGi <dudrl4612@naver.com>
2020-07-25 12:07:50 +0900

    aop

---
+++ SpringEx/src/main/java/aop001/Girl.java
@@ -0,0 +1,18 @@
+package aop001;
+
+public class Girl {
+	public void runSomething() {
+		System.out.println("열쇠로 문을 열고 집에 들어간다.");
+		
+		try {
+			System.out.println("요리를 한다.");  // 핵심관심사,  나머지는 횡단 관심사
+		} catch (Exception e) {
+			if(e.getMessage().equals("집에 불남")) {
+				System.out.println("119에 신고한다.");
+			}
+		} finally {
+			System.out.println("소등하고 잔다.");
+		}
+		System.out.println("자물쇠를 잠그로 집을 나선다.");
+	}
+}
