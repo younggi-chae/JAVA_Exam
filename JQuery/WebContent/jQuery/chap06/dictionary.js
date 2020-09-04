3171ce6c9f08d20648424533a62e9039019b997c
younggi-chae <dudrl4612@naver.com>
2020-09-04 15:50:24 +0900

    ajax

--- JQuery/WebContent/jQuery/chap06/dictionary.js
+++ JQuery/WebContent/jQuery/chap06/dictionary.js
@@ -2,7 +2,7 @@
 
 $(function(){
 	$('#letter-a a').click(function(){
-		$('#dictionary').hide().load('a.html', function(){
+		$('#dictionary').hide().load('a.html', function(){  //html 변환과정 필요x
 			$(this).fadeIn();
 		});
 		return false;
@@ -11,11 +11,13 @@ $(function(){
 
 
 //step2
-$(function(){
+/*$(function(){
 	$('#letter-b a').click(function(){
 		$.getJSON('b.json', function(data){
 			$('#dictionary').empty();
-			$.each(data, function(idx, item){
+			
+			//$(data).each(function(idx, item).... 도 가능
+			$.each(data, function(idx, item){   //콜백함수 html로 변환 작업.
 				var html = '<div class="entry">';
 				html += '<h3 class="term">' + item.term +'</h3>';
 				html += '<div class="part">' + item.part + '</div>';
@@ -27,6 +29,43 @@ $(function(){
 		});		
 		return false;
 	});
+});*/
+
+
+//step2_1
+$(function(){
+	$('#letter-b a').click(function(){
+		$('#dictionary').empty();
+		$.ajax({
+			url : 'b.json',
+			type : 'get',			
+			dataType : 'json',
+			success : successHandler2
+		});
+		return false;
+	});
+	
+	function successHandler2(data){
+		data.sort(function(a, b){
+			if(a.term < b.term){
+				return 1;
+			} else if(a.term > b.term){
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+		
+		$.each(data, function(idx, item){   //콜백함수 html로 변환 작업.
+			var html = '<div class="entry">';
+			html += '<h3 class="term">' + item.term +'</h3>';
+			html += '<div class="part">' + item.part + '</div>';
+			html += '<div class="definition">' + item.definition + '</div>';
+			html += '</div>';
+			
+			$('#dictionary').append(html);
+		});
+	}
 });
 
 
@@ -48,7 +87,7 @@ $(function(){
 		$('#dictionary').empty();
 		$.get('d.xml', function(data){  //data는  xml파일의 entrys
 			$(data).find('entry').each(function(index){
-				$entry = $(this);
+				var $entry = $(this);
 				var html = '<div class="entry">';
 				html += '<h3 class="term">' + $entry.attr('term') +'</h3>';
 				html += '<div class="part">' + $entry.attr('part') + '</div>';
@@ -61,3 +100,39 @@ $(function(){
 		return false;
 	});
 });
+
+
+//step5
+
+$(function(){
+	$('#letter-e a').click(function(){
+		$('#dictionary').empty();
+		$.get('server3.jsp', {'term' : $(this).text()}, function(data){
+			$('#dictionary').text(data);
+		});
+		return false;
+	});
+});
+
+
+//step6
+
+$(function(){
+	$('#letter-f form').submit(function(){
+		$('#dictionary').empty();
+		$.ajax({
+			url : 'server3.jsp',
+			type : 'post',
+			data : $(this).serialize(),   //this 는 form
+			dataType : 'text',
+			success : successHandler			
+		});
+		return false;
+	});
+	
+	function successHandler(data){
+		$('#dictionary').text(data);
+	}
+});
+
+
